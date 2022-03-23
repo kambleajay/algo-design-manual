62fd703b930fe989c4cc6367371c1c56a5bc3c14
Ajay Kamble <ajay.kamble@selfscore.com>
2022-03-23 23:25:12 +0530

    completed #29

--- ch1/division/core.go
+++ ch1/division/core.go
@@ -1,24 +1,40 @@
 package division
 
 import (
-	"log"
 	"math"
 )
 
-func Divide1(dividend, divisor int) int {
-	dividendAbs := math.Abs(float64(dividend))
-	divisorAbs := math.Abs(float64(divisor))
-	result := 1
-	for divisorAbs < dividendAbs {
-		result += 1
-		divisorAbs += divisorAbs
-	}
-	return result
-}
+func divide(dividend int, divisor int) int {
+	var countOfNeg, answer int
+	absDividend, absDivisor := dividend, divisor
 
-func Divide2(x, y float64) float64 {
-	diffOfLogs := math.Log(x) - math.Log(y)
-	result := math.Exp(diffOfLogs)
-	log.Printf("diff of logs: %f, e^dol = %f\n", diffOfLogs, result)
-	return result
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+
+	if dividend < 0 {
+		absDividend = -absDividend
+		countOfNeg++
+	}
+	if divisor < 0 {
+		absDivisor = -absDivisor
+		countOfNeg++
+	}
+
+	tmpDividend, tmpDivisor := absDividend, absDivisor
+	for tmpDividend >= tmpDivisor {
+		tmpAnswer := 1
+		for tmpDividend >= (tmpDivisor << 1) {
+			tmpDivisor <<= 1
+			tmpAnswer <<= 1
+		}
+		tmpDividend -= tmpDivisor
+		tmpDivisor = absDivisor
+		answer += tmpAnswer
+	}
+
+	if countOfNeg == 1 {
+		return -answer
+	}
+	return answer
 }
--- ch1/division/core_test.go
+++ ch1/division/core_test.go
@@ -1,20 +1,46 @@
 package division
 
-import "testing"
+import (
+	"math"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestDivide(t *testing.T) {
 	var tests = []struct {
-		dividend float64
-		divisor  float64
-		want     float64
+		dividend int
+		divisor  int
+		want     int
 	}{
-		{27, 9, 3},
-		{100, 10, 10},
-		{46, 5, 9},
+		{10, 3, 3},
+		{7, -3, -2},
+		{5, 5, 1},
+		{-3, 3, -1},
+		{-6, -6, 1},
+		{-2147483648, -1, 2147483647},
+		{3, 1, 3},
+		{10, 1, 10},
+		{2147483647, 1, 2147483647},
+		{2147483647, -1, -2147483647},
+		{13, 2, 6},
+		{2147483647, 2, 1073741823},
+		{-1, 1, -1},
+		{15, 2, 7},
 	}
 	for _, test := range tests {
-		if got := Divide2(test.dividend, test.divisor); got != test.want {
-			t.Errorf("%f/%f != %f (%f)\n", test.dividend, test.divisor, test.want, got)
+		if got := divide(test.dividend, test.divisor); got != test.want {
+			t.Errorf("%d/%d != %d (%d)\n", test.dividend, test.divisor, test.want, got)
 		}
+	}
+}
+
+func BenchmarkDivide(b *testing.B) {
+	seed := time.Now().UTC().UnixNano()
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < b.N; i++ {
+		dividend := rng.Intn(math.MaxInt32-math.MinInt32) + math.MinInt32
+		divisor := rng.Intn(math.MaxInt32-math.MinInt32) + math.MinInt32
+		divide(dividend, divisor)
 	}
 }
