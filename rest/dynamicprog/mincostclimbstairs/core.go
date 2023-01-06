7fbe14377b97ff15042344664e8d632cb9c6310a
Ajay Kamble <ajay.kamble@selfscore.com>
2023-01-06 17:29:00 +0530

    Completed O(1) space solution for #746

--- rest/dynamicprog/mincostclimbstairs/core.go
+++ rest/dynamicprog/mincostclimbstairs/core.go
@@ -1,5 +1,7 @@
 package mincostclimbstairs
 
+import "math"
+
 func min(x, y int) int {
 	if x < y {
 		return x
@@ -7,7 +9,7 @@ func min(x, y int) int {
 	return y
 }
 
-func minCostClimbingStairs(cost []int) int {
+func minCostClimbingStairs1(cost []int) int {
 	n := len(cost)
 	dp := make([]int, n)
 	dp[0] = cost[0]
@@ -16,4 +18,15 @@ func minCostClimbingStairs(cost []int) int {
 		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
 	}
 	return min(dp[n-1], dp[n-2])
+}
+
+func minCostClimbingStairs(cost []int) int {
+	back2, back1 := cost[0], cost[1]
+	next := math.MaxInt64
+	for i := 2; i < len(cost); i++ {
+		next = min(back2, back1) + cost[i]
+		back2 = back1
+		back1 = next
+	}
+	return min(back1, back2)
 }
